1f7d3ffc266a9ae315da94f8a1fc1a4014728903
Nikita <74094674+hahafury@users.noreply.github.com>
2023-04-04 12:53:38 +0300

    :tada: Initial commit

---
+++ app/main.ts
@@ -0,0 +1,39 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+
+import { LoggingInterceptor } from '@libs/logging/logging.interceptor';
+import { HttpExceptionFilter } from '@libs/filters/http-exception.filter';
+
+import { Config } from '@app/app.config';
+import { AppModule } from '@app/app.module';
+import helmet from 'helmet';
+import compression from 'compression';
+
+function setupSwagger(app: INestApplication): void {
+  const documentBuilder: Omit<OpenAPIObject, 'paths'> = new DocumentBuilder()
+    .setTitle('Nest.js example')
+    .setDescription('This is example for nest.js')
+    .setVersion('1.0')
+    .addBasicAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, documentBuilder);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: { defaultModelsExpandDepth: -1 },
+  });
+}
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.use(helmet());
+  app.use(compression());
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new LoggingInterceptor());
+  app.useGlobalFilters(new HttpExceptionFilter());
+  setupSwagger(app);
+  await app.listen(Config.PORT);
+}
+
+bootstrap();
